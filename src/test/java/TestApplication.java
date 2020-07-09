6a2ff67576dcbb84fb9f95f4510eed6cdbe6ecb7
huangj <xyhuangjia@gmail.com>
2020-07-09 23:58:02 +0800

    runloop

--- src/test/java/TestApplication.java
+++ src/test/java/TestApplication.java
@@ -5,13 +5,24 @@ public class TestApplication {
 
 
     public static void main(String[] args) {
-        GeneratorConfig config = GeneratorConfig.builder().jdbcUrl("jdbc:mysql://192.168.1.211:3306/example")
-                .userName("root")
-                .password("root")
-                .port(8068)
+//        GeneratorConfig config = GeneratorConfig.builder().jdbcUrl("jdbc:mysql://192.168.1.211:3306/example")
+//                .userName("root")
+//                .password("root")
+//                .port(8068)
+//                .driverClassName("com.mysql.cj.jdbc.Driver")
+//                .basePackage("com.github.davidfantasy.mybatisplus.generatorui.example")
+//                .build();
+//        MybatisPlusToolsApplication.run(config);
+        String JDBC = "jdbc:mysql://39.98.60.130:3306/mybatis_plus?characterEncoding=utf8&useSSL=false&&allowPublicKeyRetrieval=true";
+        String userName = "123";
+        String password = "654";
+        GeneratorConfig config = GeneratorConfig.builder()
+                .jdbcUrl(JDBC)
+                .userName(userName)
+                .password(password)
                 .driverClassName("com.mysql.cj.jdbc.Driver")
-                .basePackage("com.github.davidfantasy.mybatisplus.generatorui.example")
-                .build();
+                .basePackage("com.HBC.example")
+                .port(8068).build();
         MybatisPlusToolsApplication.run(config);
     }
 
